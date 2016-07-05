941ff1b55e1d863549c064828ce5eb9d8fa8e316
Rebecca Spencer-Strong <raspencerstrong@dons.usfca.edu>
2016-07-05 13:16:01 -0700

    final version with comments

--- flip.py
+++ flip.py
@@ -1,9 +1,11 @@
+#flip program for Images Project by RSS
+#initializations
 import sys
 from PIL import Image
-# define your flip function here
+
+#flips and displays an image
 def flip(img):
     width, height = img.size
-    print width, height
     imgdup = img.copy()
     matrix_orig = img.load()
     matrix_dup = imgdup.load()
@@ -12,23 +14,12 @@ def flip(img):
             matrix_dup[x,y] = matrix_orig[width-x-1,y]
     imgdup.show()
 
+#program start
 if len(sys.argv) <= 1:
     print "missing image filename"
     sys.exit(1)
 filename = sys.argv[1]
 img = Image.open(filename)
 img = img.convert("L")
-
-# call your flip function here
 img.show()
 flip(img)
-
-##hints
-## width, height = img.size
-###need width and height to iterate over the pixels of the image
-##img.copy() duplicates the image
-## write code as
-### imgdup=img.copy() creates a new picture, flipped
-##img.load() returns a two-dimensional matrix
-##set matrix to be m = img.load()
-##notation is m[x,y] or m[y][x]

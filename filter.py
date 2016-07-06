73200b7b6bae66db6eaa8264d39389dcad55b931
Rebecca Spencer-Strong <raspencerstrong@dons.usfca.edu>
2016-07-06 11:06:36 -0700

    initial add

---
+++ filter.py
@@ -0,0 +1,47 @@
+import sys
+from PIL import Image
+
+def filter(img, f):
+    width, height = img.size
+    imgdup = img.copy()
+    pixels = imgdup.load()
+    for x in range(0, width):
+        for y in range(0, height):
+            r = region3x3(img, x, y)
+            pixels[x, y] = f(r)
+    imgdup.show()
+
+#defines a pixel region surrounding a specific point
+def region3x3(img,x,y):
+    me =getpixel(img,x,y)
+    N=getpixel(img,x,y-1)
+    S=getpixel(img,x,y+1)
+    E=getpixel(img,x+1,y)
+    W=getpixel(img,x-1,y)
+    NW=getpixel(img,x-1,y-1)
+    NE=getpixel(img,x+1,y-1)
+    SE=getpixel(img,x+1,y+1)
+    SW=getpixel(img,x-1,y+1)
+    return [me,N,S,E,W,NW,NE,SE,SW]
+
+#returns a specific pixel
+def getpixel(img,x,y):
+    width,height=img.size
+    if x<0:
+        x=0
+    elif x>=width:
+        x=width-1
+    if y<0:
+        y=0
+    elif y>=height:
+        y=height-1
+    return img.load()[x,y]
+
+#introductory section
+def open(argv):
+    if len(argv)<=1:
+        print "missing image filename"
+        sys.exit(1)
+    img = Image.open(argv[1])
+    img = img.convert("L")
+    return img
